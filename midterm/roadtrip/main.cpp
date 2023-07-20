a49c17a56731096832948dcaac4a4f5cd6672cde
patrickzhou1234 <patrickchang44543@gmail.com>
2023-07-20 09:25:37 -0700

    60percent

---
+++ midterm/roadtrip/main.cpp
@@ -0,0 +1,54 @@
+// link to problem
+#include <iostream>
+
+using namespace std;
+
+#define MAX_INT 2147483647
+
+int main()
+{
+    string firstcow, secondcow;
+    int n, m, i, tmpamt, j, mxln, firstcowpos = 0, secondcowpos = 0, ct = 0;
+    char tmpdir;
+    cin >> n >> m;
+    for (i = 0; i < n; i++)
+    {
+        cin >> tmpamt >> tmpdir;
+        for (j = 0; j < tmpamt; j++)
+        {
+            firstcow += tmpdir;
+        }
+    }
+    for (i = 0; i < m; i++)
+    {
+        cin >> tmpamt >> tmpdir;
+        for (j = 0; j < tmpamt; j++)
+        {
+            secondcow += tmpdir;
+        }
+    }
+    mxln = max(firstcow.length(), secondcow.length());
+    for (i = 0; i < mxln; i++)
+    {
+        if (i < firstcow.length())
+        {
+            if (firstcow[i] == 'L')
+                firstcowpos--;
+            if (firstcow[i] == 'R')
+                firstcowpos++;
+        }
+        if (i < secondcow.length())
+        {
+            if (secondcow[i] == 'L')
+                secondcowpos--;
+            if (secondcow[i] == 'R')
+                secondcowpos++;
+        }
+        if (firstcowpos == secondcowpos)
+        {
+            ct++;
+        }
+    }
+    cout << ct;
+    return 0;
+}
