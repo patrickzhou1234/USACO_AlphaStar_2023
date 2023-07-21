46f5fa83b9ae1491e89daf3ce6fcfa1a493b6480
patrickzhou1234 <patrickchang44543@gmail.com>
2023-07-21 10:53:52 -0700

    imcomplete

---
+++ acorncollection/main.cpp
@@ -0,0 +1,20 @@
+// link to problem
+#include <iostream>
+
+using namespace std;
+
+#define MAX_INT 2147483647
+
+int main()
+{
+    int n, k, i;
+    cin >> n >> k;
+    int a[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    system("pause");
+    return 0;
+}
